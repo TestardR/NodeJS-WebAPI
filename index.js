fbaededca06a0c5571c0df27dac6dd9872066014
Romain <rmtestard@gmail.com>
2018-12-12 13:01:10 +0100

    setting up a server with express

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-var http = require('http');
+// var http = require('http');
 
-http
-  .createServer(function(req, res) {
-    res.writeHead(200, {
-      'Content-Type': 'text/plain'
-    });
-    res.end('Hello World\n');
-  })
-  .listen(3000, '127.0.0.1');
+// http
+//   .createServer(function(req, res) {
+//     res.writeHead(200, {
+//       'Content-Type': 'text/plain'
+//     });
+//     res.end('Hello World\n');
+//   })
+//   .listen(3000, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:3000');
+// console.log('Server running at http://127.0.0.1:3000');
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+  //   res.send('Hello World!');
+  res.json({ hello: 'world' });
+});
+
+var server = app.listen(3000, function() {
+  console.log('Server running at http://127.0.0.1:3000');
+});
